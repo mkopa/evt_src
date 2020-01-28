0b583b3946e952703a3887972363ebf089136b34
kwasniew <kwasniewski.mateusz@gmail.com>
2020-01-28 14:05:49 +0100

    init

---
+++ src/card.js
@@ -0,0 +1,40 @@
+module.exports = function card() {
+
+    let limit;
+    let used = 0;
+
+    // invariant
+    function limitAlreadyAssigned() {
+        return limit != null;
+    }
+
+    function notEnoughMoney(amount) {
+        return amount > availableLimit();
+    }
+
+    function availableLimit() {
+        return limit - used;
+    }
+
+    return {
+        assignLimit(amount) {
+            if(limitAlreadyAssigned()) {
+                throw new Error('Cannot assign limit for the second time');
+            }
+            limit = amount;
+        },
+        availableLimit,
+        withdraw(amount) {
+            if(!limitAlreadyAssigned()) {
+                throw new Error('No limit assigned');
+            }
+            if (notEnoughMoney(amount)) {
+                throw new Error('Not enough money');
+            }
+            used += amount;
+        },
+        repay(amount) {
+            used -= amount;
+        }
+    };
+};
