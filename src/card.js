90183ad0c54f851ee7e2e725cbf2adf6f19aee9f
kwasniew <kwasniewski.mateusz@gmail.com>
2020-01-28 15:04:45 +0100

    recreate from

--- src/card.js
+++ src/card.js
@@ -1,49 +1,72 @@
-module.exports = now => id => {
-    let limit;
-    let used = 0;
-    let events = [];
+module.exports = now => {
+    const card = id => {
+        let limit;
+        let used = 0;
+        let events = [];
 
-    // invariant
-    function limitAlreadyAssigned() {
-        return limit != null;
-    }
-
-    function notEnoughMoney(amount) {
-        return amount > availableLimit();
-    }
-
-    function availableLimit() {
-        return limit - used;
-    }
-
-    return {
-        assignLimit(amount) {
-            if(limitAlreadyAssigned()) {
-                throw new Error('Cannot assign limit for the second time');
-            }
-            events.push({type: 'LIMIT_ASSIGNED', amount, card_id: id, date: now().toJSON()});
-            limit = amount;
-        },
-        availableLimit,
-        withdraw(amount) {
-            if(!limitAlreadyAssigned()) {
-                throw new Error('No limit assigned');
-            }
-            if (notEnoughMoney(amount)) {
-                throw new Error('Not enough money');
-            }
-            events.push({type: 'CARD_WITHDRAWN', amount, card_id: id, date: now().toJSON()});
-            used += amount;
-        },
-        repay(amount) {
-            events.push({type: 'CARD_REPAID', amount, card_id: id, date: now().toJSON()});
-            used -= amount;
-        },
-        pendingEvents() {
-            return events;
-        },
-        uuid() {
-            return id;
+        // invariant
+        function limitAlreadyAssigned() {
+            return limit != null;
         }
+
+        function notEnoughMoney(amount) {
+            return amount > availableLimit();
+        }
+
+        function availableLimit() {
+            return limit - used;
+        }
+
+        return {
+            assignLimit(amount) {
+                if(limitAlreadyAssigned()) {
+                    throw new Error('Cannot assign limit for the second time');
+                }
+                events.push({type: 'LIMIT_ASSIGNED', amount, card_id: id, date: now().toJSON()});
+                limit = amount;
+            },
+            availableLimit,
+            withdraw(amount) {
+                if(!limitAlreadyAssigned()) {
+                    throw new Error('No limit assigned');
+                }
+                if (notEnoughMoney(amount)) {
+                    throw new Error('Not enough money');
+                }
+                events.push({type: 'CARD_WITHDRAWN', amount, card_id: id, date: now().toJSON()});
+                used += amount;
+            },
+            repay(amount) {
+                events.push({type: 'CARD_REPAID', amount, card_id: id, date: now().toJSON()});
+                used -= amount;
+            },
+            pendingEvents() {
+                return events;
+            },
+            apply(event) {
+                if (event.type === 'LIMIT_ASSIGNED') {
+                    limit = event.amount;
+                }
+                if (event.type === 'CARD_WITHDRAWN') {
+                    used += event.amount;
+                }
+                if (event.type === 'CARD_REPAID') {
+                    used -= event.amount;
+                }
+            },
+            uuid() {
+                return id;
+            }
+        };
     };
+    const recreateFrom = (uuid, events) => {
+        return events.reduce((card, event) => {
+            card.apply(event);
+            return card;
+        }, card(uuid));
+    };
+
+    return {card, recreateFrom};
 };
+
+
--- test/cardTest.js
+++ test/cardTest.js
@@ -1,6 +1,6 @@
 const test = require('tape');
 function now() { return new Date('August 19, 2018 23:15:30 UTC'); }
-const card = require("../src/card")(now);
+const {card, recreateFrom} = require("../src/card")(now);
 
 test("cannot assign limit for the second time", function (t) {
     const c = card();
@@ -78,6 +78,17 @@ test("new card gets a new id", function (t) {
     const c = card('1234');
 
     t.equal(c.uuid(), '1234');
+
+    t.end();
+});
+
+test("recreate card from events", function(t) {
+    const events = [{type: 'LIMIT_ASSIGNED', amount: 150000, card_id: '12345', date: '2018-08-19T23:15:30.000Z'}, {type: 'CARD_WITHDRAWN', amount: 100000, card_id: '12345', date: '2018-08-19T23:15:30.000Z'}, {type: 'CARD_REPAID', amount: 50000, card_id: '12354', date: '2018-08-19T23:15:30.000Z'}];
+    const c = recreateFrom('12345', events);
+
+    t.deepEqual(c.uuid(), '12345');
+    t.equal(c.availableLimit(), 100000);
+    t.deepEqual(c.pendingEvents(), []);
 
     t.end();
 });
