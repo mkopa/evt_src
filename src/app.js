ec691760af13ddc65e5a9eb5ce3cf9ce56077eab
kwasniew <kwasniewski.mateusz@gmail.com>
2020-01-29 09:38:27 +0100

    compose

--- src/app.js
+++ src/app.js
@@ -3,6 +3,21 @@ const now  = function() { return new Date(); };
 const {card, recreateFrom} = require('./card')(now);
 const ClientError = require('./clientError');
 
+function withErrorHandling(fn) {
+    return async function(req, res) {
+        try {
+            await fn(req.body);
+            res.status(204).send();
+        } catch (e) {
+            if (e instanceof ClientError) {
+                res.status(400).json({error: e.message});
+            }
+            console.log(e);
+            res.status(500).send();
+        }
+    };
+}
+
 module.exports = async function() {
     const app = express();
 
@@ -14,71 +29,28 @@ module.exports = async function() {
     const es = await initStore();
     const repository = require('./cardRepository')(recreateFrom, es);
 
+    function withPersistence(fn) {
+        return async (body) => {
+            const c = await repository.load(body.uuid);
+            fn(c, body);
+            await repository.save(c);
+        };
+    }
+    function handle(command) {
+        return withErrorHandling(withPersistence(command));
+    }
+
     app.use(express.json());
 
-    app.post('/limit', async function(req, res) {
-        try {
-            // load existing item or create empty card
-            const c = await repository.load(req.body.uuid);
-            // business logic
-            c.assignLimit(req.body.amount);
-            // store the card back
-            await repository.save(c);
-            // command handled, no response - we're in the command part of CQRS
-            res.status(204).send();
-        } catch (e) {
-            if (e instanceof ClientError) {
-                // invariant violation
-                res.status(400).json({error: e.message});
-            } else {
-                console.log(e);
-                // programmer error
-                res.status(500).send();
-            }
-        }
-    });
-    app.post('/withdrawal', async function(req, res) {
-        try {
-            // load existing item or create empty card
-            const c = await repository.load(req.body.uuid);
-            // business logic
-            c.withdraw(req.body.amount);
-            // store the card back
-            await repository.save(c);
-            // command handled, no response - we're in the command part of CQRS
-            res.status(204).send();
-        } catch (e) {
-            if (e instanceof ClientError) {
-                // invariant violation
-                res.status(400).json({error: e.message});
-            } else {
-                console.log(e);
-                // programmer error
-                res.status(500).send();
-            }
-        }
-    });
-    app.post('/repayment', async function(req, res) {
-        try {
-            // load existing item or create empty card
-            const c = await repository.load(req.body.uuid);
-            // business logic
-            c.repay(req.body.amount);
-            // store the card back
-            await repository.save(c);
-            // command handled, no response - we're in the command part of CQRS
-            res.status(204).send();
-        } catch (e) {
-            if (e instanceof ClientError) {
-                // invariant violation
-                res.status(400).json({error: e.message});
-            } else {
-                console.log(e);
-                // programmer error
-                res.status(500).send();
-            }
-        }
-    });
+    app.post('/limit', handle((c, body) => {
+        c.assignLimit(body.amount);
+    }));
+    app.post('/withdrawal', handle((c, body) => {
+        c.withdraw(body.amount);
+    }));
+    app.post('/repayment', handle((c, body) => {
+        c.repay(body.amount);
+    }));
     app.get('/limit/:uuid', async function (req, res) {
         const c = await repository.load(req.params.uuid);
         res.json({uuid: c.uuid(), limit: c.availableLimit()});
